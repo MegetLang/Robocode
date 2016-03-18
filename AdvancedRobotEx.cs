7a1e1468c6647ccadb936ba384b035a810bd874b
MegetLang <lassefredheim@gmail.com>
2016-03-18 17:00:43 +0100

    Prøve fikse Enemy data

---
+++ AdvancedRobotEx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Drawing;
+using Robocode;
+
+namespace PG4500_2016_Exam1.Robocode
+{
+    /// <summary>
+    /// Extended AdvancedRobot functionality for PG4500 robots.
+    /// version: 1.0
+    /// author: Tomas Sandnes - santom@westerdals.no
+    /// </summary>
+    public class AdvancedRobotEx : AdvancedRobot
+    {
+        // NOTE: The ORDER that Robocode runs is as follows: 
+        //		 Firing PITFALL: Gun fires before it turns!
+        //       (Source: http://robocode.sourceforge.net/help/physics/physics.html)
+        //       1. Battle view is (re)painted. 
+        //		 2. All robots Execute their code until they take action (and then paused). 
+        //		 3. Time is updated (time = time + 1). 
+        //		 4. All bullets move and check for collisions. This includes firing bullets. 
+        //		 5. All robots move (gun, radar, heading, acceleration, velocity, distance, in that order). 
+        //		 6. All robots perform scans (and collect team messages). 
+        //		 7. All robots are resumed to take new action. 
+        //		 8. Each robot processes its event queue.
+
+
+        // P R O P E R T I E S 
+        // -------------------
+
+        public EnemyData Enemy { get; set; }  // Stored info about our current radar target. (Wiped each round/match.)
+
+
+        // P U B L I C   M E T H O D S 
+        // ---------------------------
+
+        public AdvancedRobotEx()
+        {
+            Enemy = new EnemyData();
+        }
+
+        public override void OnRobotDeath(RobotDeathEvent deadRobot)
+        {
+            if (deadRobot.Name == Enemy.Name)
+            {
+                Enemy.Clear();
+            }
+        }
+
+        /// <summary>
+        /// If one of our bullets just hit our currently tracked target, update the stored data (Energy property) of our target.
+        /// </summary>
+        public override void OnBulletHit(BulletHitEvent hitData)
+        {
+            if (Enemy.Name == hitData.VictimName)
+            {
+                Enemy.Energy = hitData.VictimEnergy;
+            }
+        }
+    }
+}
---
+++ EnemyData.cs
@@ -0,0 +1,100 @@
+﻿using Robocode;
+using Robocode.Util;
+
+namespace PG4500_2016_Exam1.Robocode
+{
+    public class EnemyData
+    {
+        public long Time { get; set; }  // Time (turn) of currently stored scan.
+
+        // Enemy stuff
+        public string Name { get; set; }  // Name of enemy.
+        public double BearingRadians { get; set; }  // Bearing from us to enemy, in radians.
+        public double BearingDegrees { get { return Utils.ToDegrees(BearingRadians); } set { BearingRadians = Utils.ToRadians(value); } }  // Bearing from us to enemy, in degrees.
+        public double Distance { get; set; }  // Distance from us to enemy.
+        public double Energy { get; set; }  // Energy of enemy.
+        public double Velocity { get; set; }  // Velocity of enemy.
+        public double Acceleration { get; set; }  // How fast our enemy changes speed. (Calculated by comparing values over 2 scans.)
+        public double HeadingRadians { get; set; }  // Heading of enemy, in radians.
+        public double HeadingDegrees { get { return Utils.ToDegrees(HeadingRadians); } set { HeadingRadians = Utils.ToRadians(value); } }  // Heading of enemy, in degrees.
+        public double TurnRateRadians { get; set; }  // How fast our enemy turns, in radians (change of heading per turn). (Calculated by comparing values over 2 scans.)
+        public double TurnRateDegrees { get { return Utils.ToDegrees(TurnRateRadians); } set { TurnRateRadians = Utils.ToRadians(value); } }  // How fast our enemy turns, in degrees (change of heading per turn). (Calculated by comparing values over 2 scans.)
+                                                                                                                                              // P U B L I C   M E T H O D S 
+                                                                                                                                              // ---------------------------
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public EnemyData()
+        {
+            Time = 0;
+            Name = null;
+            BearingRadians = 0.0;
+            Distance = 0.0;
+            Energy = 0.0;
+            Velocity = 0.0;
+            Acceleration = 0.0;
+            HeadingRadians = 0.0;
+            TurnRateRadians = 0.0;
+        }
+
+
+        /// <summary>
+        /// Copy constructor.
+        /// </summary>
+        public EnemyData(EnemyData cloneMe)
+        {
+            Time = cloneMe.Time;
+            Name = cloneMe.Name;
+            BearingRadians = cloneMe.BearingRadians;
+            Distance = cloneMe.Distance;
+            Energy = cloneMe.Energy;
+            Velocity = cloneMe.Velocity;
+            Acceleration = cloneMe.Acceleration;
+            HeadingRadians = cloneMe.HeadingRadians;
+            TurnRateRadians = cloneMe.TurnRateRadians;
+        }
+
+
+        /// <summary>
+        /// Resets this EnemyData instance.
+        /// </summary>
+        public void Clear()
+        {
+            Time = 0;
+            Name = null;
+            BearingRadians = 0.0;
+            Distance = 0.0;
+            Energy = 0.0;
+            Velocity = 0.0;
+            Acceleration = 0.0;
+            HeadingRadians = 0.0;
+            TurnRateRadians = 0.0;
+        }
+
+
+        /// <summary>
+        /// Sets all EnemyData.
+        /// </summary>
+        public void SetEnemyData(ScannedRobotEvent newEnemyData)
+        {
+            // First we set the stuff that depends on last updates' values:
+            long deltaTime = newEnemyData.Time - Time;
+            TurnRateRadians = Utils.NormalRelativeAngle(newEnemyData.HeadingRadians - HeadingRadians) / deltaTime;
+            Acceleration = (newEnemyData.Velocity - Velocity) / deltaTime;
+
+            // General data:
+            Time = newEnemyData.Time;
+
+            // Compared-to-us data:
+            BearingRadians = newEnemyData.BearingRadians;
+            Distance = newEnemyData.Distance;
+
+            // Enemy specific data:
+            Name = newEnemyData.Name;
+            Energy = newEnemyData.Energy;
+            Velocity = newEnemyData.Velocity;
+            HeadingRadians = newEnemyData.HeadingRadians;
+        }
+    }
+}
